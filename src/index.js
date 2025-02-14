a72e4fa62a16c75e1beeafe9e7ad4f63e2649411
Kartikeya Joshi <kartikeyajoshi30@gmail.com>
2025-02-15 03:52:13 +0530

    configured for json , data from utl and cookies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 
 dotenv.config({
   path: './env '
 });
 connectDB() .then( () => {
-  console.log("Connected!");
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+  })
 })
 .catch((err) => {console.log(`${err}`)})
 
